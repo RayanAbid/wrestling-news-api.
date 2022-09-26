7110cf99f029c39b26ac090adebcc810e9ed5a6c
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-09-26 23:40:38 +0500

    Added homescreen

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const cors = require("cors");
+
+app.use(express.static("views"));
 
 // cors
 app.use(cors());
@@ -8,8 +11,20 @@ app.use(cors());
 // call routes
 const news = require("./routes/news");
 
-app.get("/", (req, res) => {
-  res.send("🤼 News API");
+app.get("/", (req, res, next) => {
+  const fileName = "./views/index.html";
+  var options = {
+    root: path.join(__dirname),
+  };
+  res.sendFile(fileName, options, function (err) {
+    if (err) {
+      next(err);
+    } else {
+      console.log("Sent:", fileName);
+      next();
+    }
+  });
+  // res.sendFile("./");
 });
 
 app.use("/news", news);
