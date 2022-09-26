ac2193c0a6a7e0d7429e33e56657fb08c3292dc9
Rayan Abid <68749736+RayanAbid@users.noreply.github.com>
2022-09-27 00:08:03 +0500

    Update news.js

--- routes/news.js
+++ routes/news.js
@@ -258,48 +258,56 @@ router.get("/get-aew-news", async (req, res) => {
   let browser = null;
   try {
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: false,
       args: ["--single-process", "--no-zygote", "--no-sandbox"],
       devtools: false,
     });
     const page = await browser.newPage();
     await page.setDefaultNavigationTimeout(0);
-    page.goto("https://www.wwe.com/news", {
+    page.goto("https://www.allelitewrestling.com/aew-news", {
       waitUntil: "domcontentloaded",
     });
-    await page.waitForSelector("div.card-copy > h2 > a > span");
+    await page.waitForSelector(
+      ".blog-post-post-list-link-hashtag-hover-color > a"
+    );
 
     console.log("Browser started and navigated");
 
     const images = await page.evaluate(() => {
       const srcs = Array.from(
-        document.querySelectorAll("div.card-image-area > div > a > div > img")
+        document.querySelectorAll(
+          ".gallery-item-visible.gallery-item.gallery-item-preloaded img"
+        )
       ).map((image) => image.getAttribute("src"));
       return srcs;
     });
 
     const dates = await page.evaluate(() => {
       return Array.from(
-        document.querySelectorAll("div.card-copy > div > time")
+        document.querySelectorAll(".post-metadata__date.time-ago")
       ).map((x) => x.innerText.trim());
     });
     const titleArr = await page.evaluate(() => {
       return Array.from(
-        document.querySelectorAll("div.card-copy > h2 > a > span")
+        document.querySelectorAll(
+          ".post-title.blog-hover-container-element-color.blog-post-post-list-title-color.blog-post-post-list-title-font"
+        )
       ).map((x) => x.innerText.trim());
     });
     const postLink = await page.evaluate(() => {
       return Array.from(
-        document.querySelectorAll("div.card-copy > h2 > a")
+        document.querySelectorAll(
+          ".blog-post-post-list-link-hashtag-hover-color > a"
+        )
       ).map((x) => x.href.trim());
     });
-    // const description = await page.evaluate(() => {
-    //   return Array.from(
-    //     document.querySelectorAll(
-    //       ".media-body.p-mb-none-child.media-margin30 p"
-    //     )
-    //   ).map((x) => x.innerText.trim());
-    // });
+    const description = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll(
+          ".blog-post-description-style-font.blog-post-post-list-description-style-font > div > div"
+        )
+      ).map((x) => x.innerText.trim());
+    });
     console.log("scrpae completed");
 
     await browser.close();
@@ -310,11 +318,11 @@ router.get("/get-aew-news", async (req, res) => {
     await titleArr.map((title, index) => {
       result.push({
         title,
-        description: "Read more...",
+        description: description[index],
         postLink: postLink[index],
         date: dates[index],
-        image: "https://www.wwe.com/" + images[index],
-        source: "wwe.com",
+        image: images[index],
+        source: "allelitewrestling.com",
       });
     });
 
