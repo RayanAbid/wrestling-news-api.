786d31dfc639abc0c28fb8d40737461394186289
Asad Bukhari <57019121+asdbukhari@users.noreply.github.com>
2022-11-02 15:45:35 +0500

    API created

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,39 @@
 const Users = require("../models/User");
 const bcrypt = require("bcrypt");
 const { sign } = require("jsonwebtoken");
+const bcryptjs = require('bcryptjs');
+const USER = require("../models/User")
 
 exports.signup = async (req, res, next) => {
-    console.log(req.body)
-    const { firstName, password, email } = req.body;
-
     try {
+        const { firstName, password, email } = req.body;
+        console.log(!email)
+        if (!email) {
+            return next({
+                code: 409,
+                error: true,
+                message: "Email is required"
+            })
+        }
+
+        if (!firstName) {
+            return next({
+                code: 409,
+                error: true,
+                message: "First Name is required"
+            })
+        }
+
+        const existingUser = await USER.findOne({ email, firstName });
+
+        if (existingUser?.email === email && existingUser?.firstName === firstName) {
+            return next({
+                code: 409,
+                error: true,
+                message: "User already exists try to register with new email"
+            })
+        }
+
         await bcrypt.hash(password, 10).then(async (hash) => {
             const newUser = new Users({
                 firstName: firstName,
@@ -18,17 +45,15 @@ exports.signup = async (req, res, next) => {
                 {
                     success: true,
                     data: {
-                        firstName: user.firstName,
-                        id: user.id,
-                        email: user.email,
-                        profilePicture: user.profilePicture,
+                        id: user.id
                     },
                 },
-                "importantsecret",
-                { expiresIn: 100 }
+                process.env.JWTSCERETE,
+                { expiresIn: 1800 }
             );
-            res.json({
+            return next({
                 success: true,
+                code: 200,
                 data: {
                     firstName: user.firstName,
                     id: user.id,
@@ -39,10 +64,93 @@ exports.signup = async (req, res, next) => {
             });
         });
     } catch (err) {
-        console.log("testing the err", err);
-        res.json({
+        return next({
+            code: 500,
             success: false,
-            message: err,
+            message: err.message,
         });
     }
-}
+};
+
+exports.login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await USER.findOne({ email }).exec();
+
+        if (!user) {
+            return next({ code: 401, error: true, message: "User not found" });
+        }
+
+        const decodedPassword = await bcrypt.compare(password, user.password);
+
+        if (!decodedPassword) {
+            return next({ code: 401, error: true, message: "Wrong email or password" });
+        }
+        const accessToken = sign({
+            success: true,
+            data: { id: user?.id }
+        }, process.env.JWTSCERETE, { expiresIn: 1800 })
+
+        return next({
+            success: true,
+            code: 200,
+            data: {
+                firstName: user.firstName,
+                id: user.id,
+                email: user.email,
+                profilePicture: user.profilePicture,
+                accessToken
+            },
+        });
+    } catch (err) {
+        return next({
+            code: 500,
+            success: false,
+            message: err.message,
+        });
+    }
+};
+
+exports.changePassword = async (req, res, next) => {
+    try {
+        const { email } = req.user;
+        const { password, newPassword } = req.body;
+
+        if (password === newPassword) {
+            return next({
+                code: 400,
+                success: false,
+                message: 'Current password and new pssword are same try different password',
+            });
+        }
+
+        const existingUser = await USER.findOne({ email }).select("password").exec();
+        const decodedPassword = await bcryptjs.compare(password, existingUser.password);
+
+        if (!decodedPassword) {
+            return next({
+                code: 400,
+                success: false,
+                message: 'Current password is incorrect',
+            });
+        }
+
+
+        await bcrypt.hash(newPassword, 10).then(async (hash) => {
+            existingUser.password = hash;
+            await existingUser.save();
+
+            return next({
+                code: 200,
+                success: true,
+                message: 'Password changed successfully!',
+            });
+        });
+    } catch (error) {
+        return next({ code: 401, error: true, message: error.message })
+    }
+
+
+
+};
--- routes/user.js
+++ routes/user.js
@@ -2,15 +2,22 @@ const router = require("express").Router();
 const rateLimit = require('express-rate-limit');
 const User = require("../models/User");
 const controller = require('../controllers');
+const isAuth = require("../middleware/isAuth");
 
 
 const limiter = rateLimit({
     windowMs: 3 * 1000, // Every 30 seconds
     max: 20,
-    message: `Too many requests to this end-point, please try again after ${3} seconds`,
+    message: `Too many requests to this end-point, please try again after ${30} seconds`,
 });
 
-router.post('/register', limiter, controller.signup)
+const regular_handler = (params, req, res, next) => {
+    return res.status(params?.code).send(params);
+};
+
+router.post('/register', limiter, controller.signup, regular_handler)
+router.post('/login', limiter, controller.login, regular_handler)
+router.post('/change-password', limiter, isAuth, controller.changePassword, regular_handler)
 
 
 
