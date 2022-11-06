742a8bf0bde8a00d4a157a98d96b230fd7c06722
Parrot-Nordstone <68749736+RayanAbid@users.noreply.github.com>
2022-11-06 20:46:38 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,18 +32,19 @@ mongoose
     app.use(useragent.express());
 
     app.get("/", (req, res, next) => {
-      const fileName = "./views/index.html";
-      var options = {
-        root: path.join(__dirname),
-      };
-      res.sendFile(fileName, options, function (err) {
-        if (err) {
-          next(err);
-        } else {
-          console.log("Sent:", fileName);
-          next();
-        }
-      });
+      // const fileName = "./views/index.html";
+      // var options = {
+      //   root: path.join(__dirname),
+      // };
+      // res.sendFile(fileName, options, function (err) {
+      //   if (err) {
+      //     next(err);
+      //   } else {
+      //     console.log("Sent:", fileName);
+      //     next();
+      //   }
+      // });
+      res.send("Hello");
       // res.sendFile("./");
     });
 
